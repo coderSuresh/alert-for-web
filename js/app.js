5127da0f0b216bd809e3071b4d37dc72547d79da
Shishir Khatiwada <119192456+Shishir98123@users.noreply.github.com>
2023-08-08 12:49:48 +0545

    with error and success popup

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+// Function to show the popup
+let popup = document.getElementById("successPopup");
+    popup = document.getElementById("errorPopup");
+
+// Function to open the popup
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+
+// Function to close the popup
+function closePopup() {
+    popup.classList.remove("open-popup");
+}  
+
+
+
+
