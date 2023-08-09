5ccb51d15248ef0a58c1f7d85168529dafa594ef
codersuresh <sd807141@gmail.com>
2023-08-09 21:24:57 +0545

    merge branch

--- dist/main.js
+++ dist/main.js
@@ -1,25 +1,27 @@
-const t = document.createElement("div");
-t.setAttribute("class", "popup");
-const s = document.createElement("img");
-s.setAttribute("class", "popup__icon");
-const n = document.createElement("h2");
-n.setAttribute("class", "popup__title");
+const e = document.createElement("div");
+e.setAttribute("class", "popup");
+const n = document.createElement("img");
+n.setAttribute("class", "popup__icon");
+const o = document.createElement("h2");
+o.setAttribute("class", "popup__title");
 const p = document.createElement("p");
 p.setAttribute("class", "popup__message");
-const e = document.createElement("button");
-e.setAttribute("class", "popup__close-btn");
-t.appendChild(s);
-t.appendChild(n);
-t.appendChild(p);
-t.appendChild(e);
-document.body.appendChild(t);
-const u = (c) => {
-  const { title: l, message: a, level: o, button: i } = c;
-  e.innerText = i, s.setAttribute("src", `/images/ic_${o}.png`), n.innerText = l, p.innerText = a, t.setAttribute("class", `popup popup--${o} popup--show`);
+const s = document.createElement("button");
+s.setAttribute("class", "popup__close-btn");
+e.appendChild(n);
+e.appendChild(o);
+e.appendChild(p);
+e.appendChild(s);
+document.body.appendChild(e);
+const i = (a) => {
+  let { title: r, message: c, level: t, button: l } = a;
+  if (!r || !c || !t || !l)
+    throw new Error("title, message, level, button are required");
+  t !== "success" && t !== "warning" && t !== "error" && (t = "success"), s.innerText = l, n.setAttribute("src", `/images/ic_${t.toLowerCase()}.png`), o.innerText = r, p.innerText = c, e.setAttribute("class", `popup popup--${t.toLowerCase()} popup--show`);
 };
-e.addEventListener("click", () => {
-  t.setAttribute("class", "popup");
+s.addEventListener("click", () => {
+  e.setAttribute("class", "popup");
 });
 export {
-  u as default
+  i as default
 };
