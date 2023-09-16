d6b9b8ce3258fb160e487174eeb06149722752ba
codersuresh <sd807141@gmail.com>
2023-09-16 11:21:37 +0545

    use unpkg instead of node_modules

--- dist/main.js
+++ dist/main.js
@@ -1,23 +1,23 @@
 const e = document.createElement("div");
 e.setAttribute("class", "popup");
-const n = document.createElement("img");
-n.setAttribute("class", "popup__icon");
+const o = document.createElement("img");
+o.setAttribute("class", "popup__icon");
 const p = document.createElement("h2");
 p.setAttribute("class", "popup__title");
 const r = document.createElement("p");
 r.setAttribute("class", "popup__message");
 const s = document.createElement("button");
 s.setAttribute("class", "popup__close-btn");
-e.appendChild(n);
+e.appendChild(o);
 e.appendChild(p);
 e.appendChild(r);
 e.appendChild(s);
 document.body.appendChild(e);
 const i = (a) => {
-  let { title: l, message: c, level: t, button: o } = a;
-  if (!l || !c || !t || !o)
+  let { title: c, message: l, level: t, button: n } = a;
+  if (!c || !l || !t || !n)
     throw new Error("title, message, level, button are required");
-  t !== "success" && t !== "warning" && t !== "error" && (t = "success"), s.innerText = o || "Close", n.setAttribute("src", `/node_modules/alert-for-web/dist/images/ic_${t.toLowerCase()}.png`), p.innerText = l, r.innerText = c, e.setAttribute("class", `popup popup--${t.toLowerCase()} popup--show`);
+  t !== "success" && t !== "warning" && t !== "error" && (t = "success"), s.innerText = n || "Close", o.setAttribute("src", `/images/ic_${t.toLowerCase()}.png`), p.innerText = c, r.innerText = l, e.setAttribute("class", `popup popup--${t.toLowerCase()} popup--show`);
 };
 s.addEventListener("click", () => {
   e.setAttribute("class", "popup");
